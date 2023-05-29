2150dfe938d7c58210a4494679502ddaee8b5f97
nicL <niceugeo@outlook.com>
2023-05-29 19:43:23 +0800

    Updating the include method for ImGui.

--- 04_GLGoing3D/main.cpp
+++ 04_GLGoing3D/main.cpp
@@ -6,9 +6,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 using namespace glm;
 using namespace std;
